c4fadb0ab5d97be75886ac123387df7350ac06ac
VECTOR <rahulpathania160404@gmail.com>
2022-10-03 23:03:53 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Material(
-        child: Center(
-          child: Container(child: Text("INSTAGRAM CLONE : : :"))),
+        child: Center(child: Container(child: Text("INSTAGRAM CLONE : : :"))),
       ),
     );
   }
