056d875b1cc0994ea81706937f71364370644635
VECTOR <rahulpathania160404@gmail.com>
2022-10-05 17:04:18 +0530

    day4

--- lib/pages/login_pages.dart
+++ lib/pages/login_pages.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,16 +8,62 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        appBar: AppBar(
-          title:Text("Login Page",
-          style: TextStyle(
-            color:Colors.black,
-           ),
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            title: Text(
+              "Login Page",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        ),
-        body: Text("HMM"),
-      ),
+          body: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_image.png",
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                "WELCOME....",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                        decoration: InputDecoration(
+                            hintText: "Enter Username",
+                            label: Text("Username"))),
+                    TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            hintText: "Enter Password",
+                            label: Text("Password"))),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print("buttonpressed");
+                },
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                style: TextButton.styleFrom(foregroundColor: Colors.blue),
+              ),
+            ],
+          )),
     );
   }
 }
